99195d93d91baa3a0757d94b2338194280f60f22
dbhc <dbch@ukr.net>
2020-01-19 15:38:52 +0200

    homeWorkLesson2

--- src/main/java/lesson2/homeWorkLesson2.java
+++ src/main/java/lesson2/homeWorkLesson2.java
@@ -2,6 +2,46 @@ package lesson2;
 
 public class homeWorkLesson2 {
     public static void main(String[] args) {
+/* 1. Создать переменную = 1 и используя инкремент и декремент напечатать:
 
+        1
+        0
+        1
+        1
+        1
+        2
+        1
+        1
+*/
+//        int var = 1;
+//
+//        System.out.println(var); // = 1
+//        System.out.println(--var); // = 0
+//        System.out.println(++var); // = 1
+//        System.out.println(var--); // = 1
+//        System.out.println(++var); // = 1
+//        System.out.println(++var); // = 2
+//        System.out.println(--var); // = 1
+//        System.out.println(var); // = 1
+
+/*2. Условно создать таблицу из столбцов и строк всех 8 примитивов и выполнить преобразование один к другому.
+Пометить "а" - автоматическое(неявное) / "пт" - приведение типов(явное) / "х" - там где невозможно приведение выполнить.
+*/
+
+        byte varByte = 10;
+        short varShort = 2500;
+        char varChar = 'c';
+        int varInt = 70;
+        long varLong = 37890;
+        float varFloat = 2.345f;
+        double varDoub = 5.678;
+        boolean varBool = false;
+
+
+
+//        double d = 3.4;
+//        int e= (int) d;
+//
+//        System.out.println(e);
     }
 }
