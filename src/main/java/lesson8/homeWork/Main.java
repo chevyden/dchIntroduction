3f27cc335808ee98dd79f4352740b4fb681257d3
DBCH <dbch@ukr.net>
2020-02-10 13:47:32 +0200

    homeWorkLesson8

--- src/main/java/lesson8/homeWork/Main.java
+++ src/main/java/lesson8/homeWork/Main.java
@@ -8,15 +8,20 @@ public class Main {
         firstHouse.setTableInLivingRoom(new Table(1.5, 0.8, "wood", "IKEA"));
         firstHouse.setTvSetInLivingRoom(new TvSet(1091.6, 634.6, "Samsung"));
         firstHouse.setChairInLivingRoom(new Chair(0.5, "IKEA", "Classic"));
-        firstHouse.setChairInDinnerRoom(new Chair(0.6, "Epicentr K", "Modern"));
-        firstHouse.setBedInBedRoom(new Bed(2.2, 2.0, "IKEA"));
+
+        firstHouse.setChairInDinnerRoom(new Chair());
+        firstHouse.getChairInDinnerRoom().setHeight(0.6);
+        firstHouse.getChairInDinnerRoom().setManufacturer("Temp");
+        firstHouse.getChairInDinnerRoom().setStyle("Electric)))");
+
+        firstHouse.setBedInBedRoom(new Bed());
+        firstHouse.getBedInBedRoom().setLength(2.2);
+        firstHouse.getBedInBedRoom().setWidth(2.0);
+        firstHouse.getBedInBedRoom().setManufacturer("Veneto LTD");
 
         System.out.println("The first house has: " + "\n" + firstHouse.getTableInLivingRoom().toString()
-                + "\n" +firstHouse.getTvSetInLivingRoom().toString()+ "\n" +firstHouse.getChairInLivingRoom().toString()
-                + "\n" +firstHouse.getChairInDinnerRoom().toString()+ "\n" +firstHouse.getBedInBedRoom().toString());
-//        VariablesFinal variablesFinal = new VariablesFinal();
-//        int first = variablesFinal.getFIRST_FINAL_VARIABLE();
-//        System.out.println(first);
+                + "\n" + firstHouse.getTvSetInLivingRoom().toString() + "\n" + firstHouse.getChairInLivingRoom().toString()
+                + "\n" + firstHouse.getChairInDinnerRoom().toString() + "\n" + firstHouse.getBedInBedRoom().toString());
 
     }
 }
