e42fba11090f7f7e166d89fc2aede581dbb6ca3c
DBCH <dbch@ukr.net>
2020-02-29 13:03:06 +0200

    homeWorkLesson13

--- src/main/java/lesson13/homeWorkLesson13/SecondClass.java
+++ src/main/java/lesson13/homeWorkLesson13/SecondClass.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.LinkedList;
 
 public class SecondClass extends FirstClass {
+    //Поля специально задал явно.
     private LinkedList<Integer> linkedListSecondClass;
     private HashSet<Long> hashSetSecondClass;
     private HashMap<String, Float> hashMapSecondClass;
